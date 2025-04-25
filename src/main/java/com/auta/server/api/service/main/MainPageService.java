b535ebaee478ad0f28b0d6480e536b7eda849e0a
5jun99 <oj041088@gmail.com>
2025-04-25 14:39:11 +0900

    feat: dashboard controller api 명세서 완료 #1

---
+++ src/main/java/com/auta/server/api/service/main/MainPageService.java
@@ -0,0 +1,11 @@
+package com.auta.server.api.service.main;
+
+import com.auta.server.api.service.main.response.DashboardResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MainPageService {
+    public DashboardResponse getDashBoardData(String email) {
+        return null;
+    }
+}
